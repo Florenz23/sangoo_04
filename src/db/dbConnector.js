5effd1e8a17c4eca56159dc9a5f574d97a8177da
Crazy Lama <lama23@Crazys-Mac-mini.local>
2017-06-11 11:43:48 +0200

    everything working so far now integrate contacts

--- src/db/dbConnector.js
+++ src/db/dbConnector.js
@@ -5,56 +5,14 @@ import Realm from 'realm'
 // Define your models and their properties
 
 import { UserList, User, ConnectUserList, ConnectData, UserData } from './db_models';
-import { ConnectDataClass, UserClass, UserDataClass } from './class_models'
 
 // Get the default Realm with support for our objects
 let realm = new Realm({schema: [UserList, User, ConnectUserList, ConnectData, UserData]});
 
-const createConnectData = (description, text) => {
-  return new ConnectDataClass(description,text)
-}
-let donny_userData_personalData_name = createConnectData('Name','Trump')
-let donny_userData_personalData_surname = createConnectData('Vorname','Donald')
-let donny_userData_personalData_phone = createConnectData('Handy','111 1111 111')
-let donny_userData_hashTagData_color = createConnectData('Lieblingsfarbe','blau')
-let donny_userData_hashTagData_hobby = createConnectData('Hobbies','Geld, Immobilien')
-let donny_userData_socialData_facebook = createConnectData('Facebook','https://web.facebook.com/DonaldTrump/?_rdc=1&_rdr')
-let donny_userData_socialData_linkedIn = createConnectData('LinkedIn','https://www.linkedin.com/in/donald-trump-jr-4454b862/?locale=de_DE')
-let donny_userData_socialData_twitter = createConnectData('Twitter','https://twitter.com/realdonaldtrump?lang=de')
+import donny12 from './db_mock_donny'
+import hillary from './db_mock_hillary'
 
-
-let donny_publicSharedData_personalData = []
-donny_publicSharedData_personalData.push(donny_userData_personalData_name)
-donny_publicSharedData_personalData.push(donny_userData_personalData_phone)
-
-let donny_publicSharedData_socialData = []
-donny_publicSharedData_socialData.push(donny_userData_socialData_facebook)
-donny_publicSharedData_socialData.push(donny_userData_socialData_twitter)
-
-let donny_publicSharedData_hashTagData = []
-donny_publicSharedData_hashTagData.push(donny_userData_hashTagData_hobby)
-donny_publicSharedData_hashTagData.push(donny_userData_hashTagData_color)
-
-let donny_userData_personalData = []
-donny_userData_personalData.push(donny_userData_personalData_surname)
-donny_userData_personalData.push(donny_userData_personalData_name)
-donny_userData_personalData.push(donny_userData_personalData_phone)
-
-let donny_userData_socialData = []
-donny_userData_socialData.push(donny_userData_socialData_facebook)
-donny_userData_socialData.push(donny_userData_socialData_linkedIn)
-donny_userData_socialData.push(donny_userData_socialData_twitter)
-
-let donny_userData_hashTagData = []
-donny_userData_hashTagData.push(donny_userData_hashTagData_hobby)
-donny_userData_hashTagData.push(donny_userData_hashTagData_color)
-
-let donny_userDataClass = new UserDataClass(donny_userData_personalData, donny_userData_socialData, donny_userData_hashTagData)
-let donny_publicSharedDataClass = new UserDataClass(donny_publicSharedData_socialData, donny_publicSharedData_socialData, donny_publicSharedData_hashTagData)
-
-let donny12 = new UserClass('a1',donny_userDataClass,donny_publicSharedDataClass,[])
-
-const insertUserInDb = (user) => {
+const insertUserInDb = (user,id) => {
   realm.write(() => {
     let userData = realm.create('UserData' , {
       personalData : user.userData.personalData,
@@ -67,7 +25,7 @@ const insertUserInDb = (user) => {
       hashTagData : user.publicSharedData.hashTagData
     })
     let donny = realm.create('User', {
-        userId: 'a1',
+        userId: id,
     })
     donny.userData.push(userData)
     donny.publicSharedData.push(sharedData)
@@ -75,7 +33,8 @@ const insertUserInDb = (user) => {
 
 }
 const createDummyUser = () => {
-  insertUserInDb(donny12)
+  insertUserInDb(donny12,'a1')
+  insertUserInDb(hillary,'a2')
 }
 
 const deleteUsers = () => {
---
+++ src/db/db_mock_hillary.js
@@ -0,0 +1,48 @@
+import { ConnectDataClass, UserClass, UserDataClass } from './class_models'
+
+
+const createConnectData = (description, text) => {
+  return new ConnectDataClass(description,text)
+}
+let hillary_userData_personalData_name = createConnectData('Name','Clinton')
+let hillary_userData_personalData_surname = createConnectData('Vorname','Hillary')
+let hillary_userData_personalData_phone = createConnectData('Handy','2341234 12341234')
+let hillary_userData_hashTagData_color = createConnectData('Aufenthaltsort','Gefängnis')
+let hillary_userData_hashTagData_hobby = createConnectData('Interessen','Geld Korruption')
+let hillary_userData_socialData_facebook = createConnectData('Facebook','https://web.facebook.com/DonaldTrump/?_rdc=1&_rdr')
+let hillary_userData_socialData_linkedIn = createConnectData('LinkedIn','https://www.linkedin.com/in/donald-trump-jr-4454b862/?locale=de_DE')
+let hillary_userData_socialData_twitter = createConnectData('Twitter','https://twitter.com/realdonaldtrump?lang=de')
+
+
+let hillary_publicSharedData_personalData = []
+hillary_publicSharedData_personalData.push(hillary_userData_personalData_name)
+hillary_publicSharedData_personalData.push(hillary_userData_personalData_phone)
+
+let hillary_publicSharedData_socialData = []
+hillary_publicSharedData_socialData.push(hillary_userData_socialData_facebook)
+hillary_publicSharedData_socialData.push(hillary_userData_socialData_twitter)
+
+let hillary_publicSharedData_hashTagData = []
+hillary_publicSharedData_hashTagData.push(hillary_userData_hashTagData_hobby)
+hillary_publicSharedData_hashTagData.push(hillary_userData_hashTagData_color)
+
+let hillary_userData_personalData = []
+hillary_userData_personalData.push(hillary_userData_personalData_surname)
+hillary_userData_personalData.push(hillary_userData_personalData_name)
+hillary_userData_personalData.push(hillary_userData_personalData_phone)
+
+let hillary_userData_socialData = []
+hillary_userData_socialData.push(hillary_userData_socialData_facebook)
+hillary_userData_socialData.push(hillary_userData_socialData_linkedIn)
+hillary_userData_socialData.push(hillary_userData_socialData_twitter)
+
+let hillary_userData_hashTagData = []
+hillary_userData_hashTagData.push(hillary_userData_hashTagData_hobby)
+hillary_userData_hashTagData.push(hillary_userData_hashTagData_color)
+
+let hillary_userDataClass = new UserDataClass(hillary_userData_personalData, hillary_userData_socialData, hillary_userData_hashTagData)
+let hillary_publicSharedDataClass = new UserDataClass(hillary_publicSharedData_socialData, hillary_publicSharedData_socialData, hillary_publicSharedData_hashTagData)
+
+let hillary12 = new UserClass('a1',hillary_userDataClass,hillary_publicSharedDataClass,[])
+
+export default hillary12
